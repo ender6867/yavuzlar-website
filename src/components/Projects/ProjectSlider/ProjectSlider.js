36e17e5b7218913b102d6b61008664dd0a728174
mozlercelik <celikmehmetozler@gmail.com>
2024-04-10 00:18:59 +0300

    Slider stable fix

--- src/components/Projects/ProjectSlider/ProjectSlider.js
+++ src/components/Projects/ProjectSlider/ProjectSlider.js
@@ -20,22 +20,22 @@ import Link from "next/link";
 
 const projects = [
   {
-    id: "4",
-    title: "CodeInLab",
-    description:
-      "Yazılım dillerini pratik bir şekilde deneyimleyebilecekleri ve yazılım geliştirme becerilerini geliştirebilecekleri laboratuvar ortamıdır.",
-    image: "/logo.png",
-    gitImg: "/coming-soon3.png",
-    path: "",
-  },
-  {
-    id: "2",
+    id: "1",
     title: "VulnLab",
     description:
       "Web zafiyetlerinin uygulamalı olarak öğrenilebileceği web uygulama güvenliği laboratuvar ortamıdır.",
     image: "/vulnlab.png",
     gitImg: "/gitHub.svg",
     path: "http://yavuzlar.org/vulnlab",
+  },
+  {
+    id: "2",
+    title: "CodeInLab",
+    description:
+      "Yazılım dillerini pratik bir şekilde deneyimleyebilecekleri ve yazılım geliştirme becerilerini geliştirebilecekleri laboratuvar ortamıdır.",
+    image: "/logo.png",
+    gitImg: "/coming-soon3.png",
+    path: "",
   },
   {
     id: "3",
@@ -47,7 +47,7 @@ const projects = [
     path: "",
   },
   {
-    id: "5",
+    id: "4",
     title: "VulnWeb",
     description:
       "Web uygulama güvenliği zafiyetlerinin pratiğinin yapılabileceği gerçek bir website simülasyon laboratuvarı.",
@@ -68,161 +68,226 @@ export default function OurProjects() {
   };
 
   return (
-    <Swiper
-      effect={"coverflow"}
-      grabCursor={true}
-      centeredSlides={true}
-      loop={false}
-      slidesPerView={2}
-      initialSlide={1}
-      coverflowEffect={{
-        rotate: 0,
-        stretch: 20,
-        depth: 100,
-        modifier: 2,
-        slideShadows: true,
-      }}
-      // pagination={{
-      //   el: ".swiper-pagination",
-      //   clickable: true,
-      //   dynamicBullets: true,
-      // }}
-      // pagination={pagination}
-      navigation={{
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-        clickable: true,
-      }}
-      modules={[EffectCoverflow, Pagination, Navigation]}
-      className="swiper_container"
-      onSlideChange={(swiperCore) => {
-        const { realIndex } = swiperCore;
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        "& .swiper": {
+          width: "100%",
+          pb: "40px"
+        },
+        "& .swiper-wrapper": {
+          // width: "100%",
+          // transform: "translateX(0px) !important",
+          // justifyContent: "center",
+        },
+        "& .swiper-slide": {
+          // width: "100% !important",
+          // maxWidth: "400px",
+        },
 
-        setActiveSlide(realIndex);
+
+        "& .swiper-pagination-bullet": {
+          width: "24px",
+          height: "24px",
+          textAlign: "center",
+          fontSize: "14px",
+          lineHeight: "24px",
+          color: "#fff",
+          opacity: 1,
+          border: "1px solid rgba(115, 205, 7, 1)",
+          // background: "rgba(115, 205, 7, 0.4)",
+        },
+
+        "& .swiper-pagination": {
+          position: "relative",
+          mt: "16px",
+          justifyContent: "center",
+          display: "flex",
+          flexWrap: "wrap",
+          padding: "2px 40px",
+          gap: "4px",
+        },
+
+        "& .swiper-pagination-bullet-active": {
+          color: "#fff",
+          border: "1px solid #73CD07",
+          background: "rgba(115, 205, 7, 0.5)",
+        },
+
+        "& .slider-controller": {
+          ...(
+            isMobile ? {
+              position: "absolute",
+              width: "100%",
+              mt: projects?.length > 7 ? "-32px" : "-24px",
+            } : {}
+          )
+        }
       }}
-      style={{ maxWidth: "1100px" }}
     >
-      {projects.map((project, index) => {
-        // let mtValue = project.gitImg.startsWith("/coming") ? "53px" : "0px";
-        let mtValue;
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={isMobile ? 1 : 3}
+        initialSlide={0}
+        spaceBetween={0}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 50,
+          depth: 150,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        // pagination={{
+        //   el: ".swiper-pagination",
+        //   clickable: true,
+        //   dynamicBullets: true,
+        // }}
+        pagination={pagination}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+        onSlideChange={(swiperCore) => {
+          const {
+            realIndex,
+          } = swiperCore;
 
-        isMobile
-          ? (mtValue = project.gitImg.startsWith("/coming") ? "35px" : "0px")
-          : (mtValue = project.gitImg.startsWith("/coming") ? "53px" : "0px");
+          setActiveSlide(realIndex);
+        }}
+      >
+        {projects.map((project, index) => {
+          // let mtValue = project.gitImg.startsWith("/coming") ? "53px" : "0px";
+          let mtValue;
 
-        let hwValue = isMobile ? "40" : "60";
+          isMobile
+            ? (mtValue = project.gitImg.startsWith("/coming") ? "35px" : "0px")
+            : (mtValue = project.gitImg.startsWith("/coming") ? "53px" : "0px");
 
-        return (
-          <SwiperSlide key={project.id} className="swiper-slide">
-            <Card
-              sx={{
-                width: "100%",
-                borderRadius: "20px",
-                backgroundColor: "transparent",
-                background: "rgba(255,255,255,0.1)",
-                backdropFilter: "blur(10px)",
-                filter: activeSlide != index ? "blur(4px)" : "blur(0px)",
-              }}
-            >
-              <CardContent
+          let hwValue = isMobile ? "40" : "60";
+
+          let isSlideActive = (activeSlide == index) || (activeSlide == 0 ? index == 3 || index == 1 : activeSlide == projects.length - 1 ? index == 2 || index == 0 : index == activeSlide + 1 || index == activeSlide - 1)
+
+          return (
+            <SwiperSlide key={project.id} className="swiper-slide">
+              <Card
                 sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  flexDirection: "column",
-                  py: "64px !important",
-                  gap: "40px",
-                  px: 0,
+                  display: isSlideActive ? "block" : "none",
+                  width: "100%",
+                  height: "580px",
+                  borderRadius: "20px",
+                  backgroundColor: "transparent",
+                  background: "rgba(255,255,255,0.1)",
+                  backdropFilter: "blur(10px)",
+                  filter: activeSlide != index ? "blur(4px)" : "blur(0px)",
                 }}
               >
-                <Box
+                <CardContent
                   sx={{
-                    width: { xs: "150px", md: "250px" },
-                    height: { xs: "150px", md: "250px" },
-                  }}
-                >
-                  <CardMedia
-                    sx={{ borderRadius: "20px" }}
-                    component="img"
-                    image={project.image}
-                  />
-                </Box>
-                <Box
-                  sx={{
-                    width: "100%",
-                    position: "relative",
                     display: "flex",
                     alignItems: "center",
-
-                    "& .MuiDivider-root": {
-                      borderColor: "#73CD07",
-                    },
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    py: "64px !important",
+                    gap: "40px",
+                    px: 0,
                   }}
                 >
-                  {project.path ? (
-                    <Link
-                      href={project.path}
-                      target="_blank"
-                      style={{
-                        marginTop: mtValue,
-                        position: "absolute",
-                        right: "10%",
-                      }}
-                    >
+                  <Box
+                    sx={{
+                      width: { xs: "150px", md: "250px" },
+                      height: { xs: "150px", md: "250px" },
+                    }}
+                  >
+                    <CardMedia
+                      sx={{ borderRadius: "20px" }}
+                      component="img"
+                      image={project.image}
+                    />
+                  </Box>
+                  <Box
+                    sx={{
+                      width: "100%",
+                      position: "relative",
+                      display: "flex",
+                      alignItems: "center",
+
+                      "& .MuiDivider-root": {
+                        borderColor: "#73CD07",
+                      },
+                    }}
+                  >
+                    {project.path ? (
+                      <Link
+                        href={project.path}
+                        target="_blank"
+                        style={{
+                          marginTop: mtValue,
+                          position: "absolute",
+                          right: "10%",
+                        }}
+                      >
+                        <img
+                          src={project.gitImg}
+                          alt="image"
+                          height={hwValue}
+                          width={hwValue}
+                        />
+                      </Link>
+                    ) : (
                       <img
                         src={project.gitImg}
                         alt="image"
                         height={hwValue}
                         width={hwValue}
+                        style={{
+                          marginTop: mtValue,
+                          position: "absolute",
+                          right: "10%",
+                        }}
                       />
-                    </Link>
-                  ) : (
-                    <img
-                      src={project.gitImg}
-                      alt="image"
-                      height={hwValue}
-                      width={hwValue}
-                      style={{
-                        marginTop: mtValue,
-                        position: "absolute",
-                        right: "10%",
-                      }}
-                    />
-                  )}
+                    )}
 
-                  <Box sx={{ width: "100%" }}>
-                    <Divider />
+                    <Box sx={{ width: "100%" }}>
+                      <Divider />
+                    </Box>
                   </Box>
-                </Box>
-                <Box
-                  sx={{ display: "flex", flexDirection: "column", gap: "8px" }}
-                >
-                  <Typography
-                    className="title"
-                    style={{
-                      textAlign: "center",
-                    }}
+                  <Box
+                    sx={{ display: "flex", flexDirection: "column", gap: "8px" }}
                   >
-                    {project.title}
-                  </Typography>
-                  <Typography
-                    sx={{
-                      textAlign: "center",
-                      maxWidth: "40ch",
-                    }}
-                    variant="body2"
-                    color="white"
-                  >
-                    {project.description}
-                  </Typography>
-                </Box>
-              </CardContent>
-            </Card>
-          </SwiperSlide>
-        );
-      })}
-      {isMobile ? null : (
-        <Box className="slider-controler">
+                    <Typography
+                      className="title"
+                      style={{
+                        textAlign: "center",
+                      }}
+                    >
+                      {project.title}
+                    </Typography>
+                    <Typography
+                      sx={{
+                        textAlign: "center",
+                        maxWidth: "40ch",
+                      }}
+                      variant="body2"
+                      color="white"
+                    >
+                      {project.description}
+                    </Typography>
+                  </Box>
+                </CardContent>
+              </Card>
+            </SwiperSlide>
+          );
+        })}
+
+        <Box className="slider-controller">
           <Box
             sx={{ color: "#73CD07 !important", px: "10px" }}
             className="swiper-button-prev slider-arrow"
@@ -233,7 +298,7 @@ export default function OurProjects() {
           />
           {/* <Box className="swiper-pagination" /> */}
         </Box>
-      )}
-    </Swiper>
+      </Swiper>
+    </Box>
   );
 }
